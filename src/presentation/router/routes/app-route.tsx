4cf5d73320badd750f1b90848ff7468eff488ce2
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-05-22 14:46:38 -0300

    chore: configure route

--- src/presentation/router/routes/app-route.tsx
+++ src/presentation/router/routes/app-route.tsx
@@ -1,6 +1,7 @@
 import { DashboardPage } from '@presentation/pages/dashboard';
 import { AppRoute } from '../types/app-route';
-import { CoreCompanyPage } from '@presentation/pages/core/company';
+import { CorePage } from '@presentation/pages/core';
+import { CompanyPageFactory } from '../../../core/factories/core/company-page';
 
 /**
  * ROUTES = single source of truth for:
@@ -23,18 +24,36 @@ export const routes: AppRoute[] = [
     labelKey: 'sidebar_dashboard',
     showInSidebar: true,
   },
+
+  /**
+   * CULINARY MODULE
+   */
   {
     path: '/core',
-    labelKey: 'core',
+    element: <CorePage />,
+    labelKey: 'sidebar_core',
+    showInSidebar: true,
     isGroupRoute: true,
-    showInSidebar: false,
     children: [
       {
         path: 'companies',
-        labelKey: 'core_companies',
-        permission: 'core.company.view',
+        labelKey: 'sidebar_core_companies',
         showInSidebar: true,
-        element: <CoreCompanyPage />,
+        permission: 'core.company.view',
+        element: <CompanyPageFactory />,
+      },
+
+      {
+        path: 'companies/new',
+        element: <div>Company Create</div>,
+        permission: 'core.company.create',
+        showInSidebar: false,
+      },
+      {
+        path: 'companies/edit/:id',
+        element: <div>Company Edit</div>,
+        permission: 'core.company.update',
+        showInSidebar: false,
       },
     ],
   },
