39b8a0fa303bcaaa1517a3f133771409ffb1c3e9
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-05-25 20:48:36 -0300

    chore: use factory

--- src/main/factories/core/company-page.tsx
+++ src/main/factories/core/company-page.tsx
@@ -1,28 +1,13 @@
-import { DefaultAuthHttpClient } from '@infra/base/http/clients/default-auth-http-client';
-import { FetchHttpClient } from '@infra/base/http/clients/fetch-http-client';
-import { LocalStorage } from '@infra/base/http/clients/local-storage';
-import { CompanyService } from '@infra/core/services/company-service';
-import { DefaultAuthService } from '@infra/base/security/services/default-auth-service';
-import { JwtAuthorizationProvider } from '@infra/base/security/providers/jwt-authorization-provider';
 import { CoreCompanyPage } from '@presentation/core/pages/company';
 import { JSX } from 'react';
+import { makeCompanyService } from './services/make-company-service';
+import { makeAuthorizationProvider } from '@infra/base/provider/make-authorization-provider';
 
 export const CompanyPageFactory = (): JSX.Element => {
-  const httpClient = new FetchHttpClient();
-  const storage = new LocalStorage();
-  const authService = new DefaultAuthService(httpClient);
-  const authorizationProvider = new JwtAuthorizationProvider(storage);
-  const authClient = new DefaultAuthHttpClient(
-    httpClient,
-    storage,
-    process.env.API_URL,
-    authService,
-  );
-  const companyService = new CompanyService(authClient);
   return (
     <CoreCompanyPage
-      companyService={companyService}
-      authorizationProvider={authorizationProvider}
+      companyService={makeCompanyService()}
+      authorizationProvider={makeAuthorizationProvider()}
     />
   );
 };
