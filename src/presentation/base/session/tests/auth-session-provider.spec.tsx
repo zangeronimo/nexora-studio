f31db59ddf131bf48b983839fdee24ed8b16173c
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-06-01 20:36:23 -0300

    test: add i18n and toast provider

--- src/presentation/base/session/tests/auth-session-provider.spec.tsx
+++ src/presentation/base/session/tests/auth-session-provider.spec.tsx
@@ -1,7 +1,9 @@
 import { IUserProfileService } from '@application/core/contracts/user-profile-service';
 import { AuthContext } from '@presentation/base/auth/auth-context';
+import { I18nProvider } from '@presentation/base/i18n/hooks/i18n-provider';
 import { AuthSessionProvider } from '@presentation/base/session/auth-session-provider';
 import { useAuthSession } from '@presentation/base/session/use-auth-session';
+import { ToastProvider } from '@presentation/base/toast/provider/toast-provider';
 import { renderHook, waitFor } from '@testing-library/react';
 
 describe('AuthSessionProvider', () => {
@@ -32,17 +34,25 @@ describe('AuthSessionProvider', () => {
       uploadAvatar: jest.fn(),
     };
 
+    const localeResolver = {
+      resolve: jest.fn(),
+    };
+
     const wrapper = ({ children }: any) => (
-      <AuthContext.Provider
-        value={{
-          authenticated: true,
-          setAuthenticated: jest.fn(),
-        }}
-      >
-        <AuthSessionProvider service={service as IUserProfileService}>
-          {children}
-        </AuthSessionProvider>
-      </AuthContext.Provider>
+      <I18nProvider localeResolver={localeResolver}>
+        <ToastProvider>
+          <AuthContext.Provider
+            value={{
+              authenticated: true,
+              setAuthenticated: jest.fn(),
+            }}
+          >
+            <AuthSessionProvider service={service as IUserProfileService}>
+              {children}
+            </AuthSessionProvider>
+          </AuthContext.Provider>
+        </ToastProvider>
+      </I18nProvider>
     );
 
     renderHook(() => useAuthSession(), {
@@ -80,17 +90,25 @@ describe('AuthSessionProvider', () => {
       get: jest.fn().mockResolvedValue(session),
     };
 
+    const localeResolver = {
+      resolve: jest.fn(),
+    };
+
     const wrapper = ({ children }: any) => (
-      <AuthContext.Provider
-        value={{
-          authenticated: true,
-          setAuthenticated: jest.fn(),
-        }}
-      >
-        <AuthSessionProvider service={service as any}>
-          {children}
-        </AuthSessionProvider>
-      </AuthContext.Provider>
+      <I18nProvider localeResolver={localeResolver}>
+        <ToastProvider>
+          <AuthContext.Provider
+            value={{
+              authenticated: true,
+              setAuthenticated: jest.fn(),
+            }}
+          >
+            <AuthSessionProvider service={service as any}>
+              {children}
+            </AuthSessionProvider>
+          </AuthContext.Provider>
+        </ToastProvider>
+      </I18nProvider>
     );
 
     const { result } = renderHook(() => useAuthSession(), {
@@ -107,17 +125,25 @@ describe('AuthSessionProvider', () => {
       get: jest.fn(),
     };
 
+    const localeResolver = {
+      resolve: jest.fn(),
+    };
+
     const wrapper = ({ children }: any) => (
-      <AuthContext.Provider
-        value={{
-          authenticated: false,
-          setAuthenticated: jest.fn(),
-        }}
-      >
-        <AuthSessionProvider service={service as any}>
-          {children}
-        </AuthSessionProvider>
-      </AuthContext.Provider>
+      <I18nProvider localeResolver={localeResolver}>
+        <ToastProvider>
+          <AuthContext.Provider
+            value={{
+              authenticated: false,
+              setAuthenticated: jest.fn(),
+            }}
+          >
+            <AuthSessionProvider service={service as any}>
+              {children}
+            </AuthSessionProvider>
+          </AuthContext.Provider>
+        </ToastProvider>
+      </I18nProvider>
     );
 
     renderHook(() => useAuthSession(), {
